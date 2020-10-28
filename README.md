457b23e9009782526a9aa91ad3e46a3fcd96c946
kimchiswap <dev@kimchiswap.io>
2020-10-28 11:00:00 +0500

    Updated code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# @kimchiswap/liquidity-staker
+# @kswap/liquidity-staker
 
 Forked from 
 [https://github.com/Synthetixio/synthetix/tree/v2.27.2/](https://github.com/Synthetixio/synthetix/tree/v2.27.2/)
--- test/fixtures.ts
+++ test/fixtures.ts
@@ -4,7 +4,7 @@ import { solidity, deployContract } from 'ethereum-waffle'
 
 import { expandTo18Decimals } from './utils'
 
-import KimchiswapV2ERC20 from '@kimchiswap/v2-core/build/ERC20.json'
+import KswapV2ERC20 from '@kimchiswap/v2-core/build/ERC20.json'
 import TestERC20 from '../build/TestERC20.json'
 import StakingRewards from '../build/StakingRewards.json'
 import StakingRewardsFactory from '../build/StakingRewardsFactory.json'
@@ -22,7 +22,7 @@ interface StakingRewardsFixture {
 export async function stakingRewardsFixture([wallet]: Wallet[]): Promise<StakingRewardsFixture> {
   const rewardsDistribution = wallet.address
   const rewardsToken = await deployContract(wallet, TestERC20, [expandTo18Decimals(1000000)])
-  const stakingToken = await deployContract(wallet, KimchiswapV2ERC20, [expandTo18Decimals(1000000)])
+  const stakingToken = await deployContract(wallet, KswapV2ERC20, [expandTo18Decimals(1000000)])
 
   const stakingRewards = await deployContract(wallet, StakingRewards, [
     rewardsDistribution,
